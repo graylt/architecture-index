21d2fad287deb79188e2e018a2cb8fc5ebf90413
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-12-28 18:24:18 -0500

    updated working crud routes

---
+++ models/architecture.js
@@ -0,0 +1,92 @@
+module.exports = [
+
+    {
+        title: "Maison La Roche",
+        architectOne: "Le Corbusier",
+        architectTwo: "Pierre Jeanneret",
+        year: "1953-59",
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Villa_La_Roche_2013.jpg/2560px-Villa_La_Roche_2013.jpg",
+        loc: { 
+            streetAddress: "8-10 Square du Docteur-Blanche", 
+            city: "Paris", 
+            postalCode: 75016, 
+            country: "France"
+        },
+        link: "foundationlecorbusier.fr",
+        dayTag: "☀", 
+        visits: 1
+    },
+    {
+        title: "Sainte-Marie de La Tourette",
+        architectOne: "Le Corbusier",
+        year: "1953-60",
+        img: "https://upload.wikimedia.org/wikipedia/en/d/d8/Sainte_Marie_de_La_Tourette_2007.jpg",
+        loc: { 
+            streetAddress: "Route de la Tourette", 
+            city: "Éveux", 
+            postalCode: 69210, 
+            country: "France"
+        },
+        link: "couventdelatourette.fr",
+        dayTag: "☀",
+        nightTag: "🛏"
+    },
+    {
+        title: "E-1027 & Le Cabanon",
+        architectOne: "Eileen Gray",
+        architectTwo: "Le Corbusier",
+        year: "1929 & 1951",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Sea_shore_at_Roquebrune.jpg/2560px-Sea_shore_at_Roquebrune.jpg",
+        loc: { 
+            streetAddress: "Sentier Massolin", 
+            city: "Roquebrun-Cap-Martin", 
+            postalCode: "06190",
+            country: "France"
+        },
+        link: "capmoderne.com",
+        dayTag: "☀",
+        nightTag: "🛏"
+    },
+    {
+        title: "Les Arcs",
+        architectOne: "Charlotte Perriand",
+        year: "1968",
+        img: "https://upload.wikimedia.org/wikipedia/commons/d/dd/Arcs_1950.jpg",
+        loc: { 
+            streetAddress: "Les Arcs", 
+            city: "Bourg-Saint-Maurice", 
+            postalCode: "73700",
+            country: "France"
+        },
+        link: "lesarcs.com",
+        nightTag: "🛏"
+    },
+    {
+        title: "Villa Necchi Campiglio",
+        architectOne: "Piero Portaluppi",
+        year: "1932-35",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Villa_necchi_campiglio%2C_esterno_01.jpg/2560px-Villa_necchi_campiglio%2C_esterno_01.jpg",
+        loc: { 
+            streetAddress: "14 Via Mozart", 
+            city: "Milan", 
+            postalCode: "20122",
+            country: "Italy"
+        },
+        link: "fondoambiente.it",
+        dayTag: "☀"
+    },
+    {
+        title: "Finn Juhl House",
+        architectOne: "Finn Juhl",
+        year: "1942",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Finn_Juhls_hus_01.JPG/2880px-Finn_Juhls_hus_01.jpg",
+        loc: { 
+            streetAddress: "15 Kratvaenget", 
+            city: "Charlottenlund", 
+            postalCode: "2920",
+            country: "Denmark"
+        },
+        link: "ordrupgaard.dk",
+        dayTag: "☀"
+    }
+    ]
---
+++ models/schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const architectureSchema = new Schema ({
+
+    title: {type: String},
+    architectOne: {type: String},
+    architectTwo: {type: String},
+    year: {type:String},
+    img: [{type: String}],
+    loc: {streetAddress: String, city: String, state: String, postalCode: String, country: String},
+    link: {type: String},
+    dayTag: {type: String},
+    nightTag: {type: String},
+    visits: {type: Number}
+})
+
+
+    
+const Architecture = mongoose.model('Architecture', architectureSchema);
+module.exports = Architecture;
